bedd6326ad4793776c0b55748389db304db98d23
Kannekanti, Mohan <mohan.kannekanti@gmail.com>
2019-12-27 12:06:06 -0800

    Longest Common Prefix Problem

---
+++ java/14.Longest Common Prefix/Solution.java
@@ -0,0 +1,73 @@
+/* 
+
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+Example 1:
+
+Input: ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+Note:
+
+All given inputs are in lowercase letters a-z.
+
+*/
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";    
+            return longestCommonPrefix(strs, 0 , strs.length - 1);
+    }
+
+    private String longestCommonPrefix(String[] strs, int l, int r) {
+        if (l == r) {
+            return strs[l];
+        }
+        else {
+            int mid = (l + r)/2;
+            String lcpLeft =   longestCommonPrefix(strs, l , mid);
+            String lcpRight =  longestCommonPrefix(strs, mid + 1,r);
+            return commonPrefix(lcpLeft, lcpRight);
+        }
+    }
+
+    String commonPrefix(String left,String right) {
+        int min = Math.min(left.length(), right.length());       
+        for (int i = 0; i < min; i++) {
+            if ( left.charAt(i) != right.charAt(i) )
+                return left.substring(0, i);
+        }
+        return left.substring(0, min);
+    }    
+}
+
+public class MainClass {
+    public static String[] stringToStringArray(String line) {
+        JsonArray jsonArray = JsonArray.readFrom(line);
+        String[] arr = new String[jsonArray.size()];
+        for (int i = 0; i < arr.length; i++) {
+          arr[i] = jsonArray.get(i).asString();
+        }
+        return arr;
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            String[] strs = stringToStringArray(line);
+            
+            String ret = new Solution().longestCommonPrefix(strs);
+            
+            String out = (ret);
+            
+            System.out.print(out);
+        }
+    }
+}
